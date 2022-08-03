27e609f1e677d60b64ae48504ed2aed19b8a756a
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-03 14:18:39 -0400

    adding category, inde, and product js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,19 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+
+    },
+    category_new: {
+      type: DataTypes.STRING,
+      allowNull: false
+
+    }
+    
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignkey: 'category_id',
+});
 
 // Categories have many Products
+Category.haveMany(Product, {
+  foreignkey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  through: ProductTag,
+  foreignkey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignkey: 'tag_id',
+})
 
 module.exports = {
   Product,
